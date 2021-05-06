371b85d528dc6f2b6190a5291e943a8a54877eb9
Suraj Nanda <50697016+Surajnanda334@users.noreply.github.com>
2021-05-06 17:57:30 +0530

    final commit

--- bubbleSort.cpp
+++ bubbleSort.cpp
@@ -1,5 +1,5 @@
 // Bubble sort
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
 
 // Function to swap two numbers
@@ -14,12 +14,23 @@ void swap(int *xp, int *yp)
 void bubbleSort(int arr[], int n)
 {
 	int i, j;
-	for (i = 0; i < n-1; i++)	
-	
-	// Last i elements are already in place
-	for (j = 0; j < n-i-1; j++)
-		if (arr[j] > arr[j+1])
-			swap(&arr[j], &arr[j+1]);
+   bool swapped;
+   for (i = 0; i < n-1; i++)
+   {
+     swapped = false;
+     for (j = 0; j < n-i-1; j++)
+     {
+        if (arr[j] > arr[j+1])
+        {
+           swap(&arr[j], &arr[j+1]);
+           swapped = true;
+        }
+     }
+  
+     // IF no two elements were swapped by inner loop, then break
+     if (swapped == false)
+        break;
+   }
 }
 
   // Function to print an array 
